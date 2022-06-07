cda20c5415d0682c7ce869809e664838e0c62ccf
alabaganne <alabaganne9@gmail.com>
2022-06-07 11:24:16 +0100

    Fix auth bug web app

--- client/app.js
+++ client/app.js
@@ -5,7 +5,7 @@ const protectedRoutes = [
 
 var user = null;
 function init() {
-	user = JSON.parse(localStorage.getItem('user'));
+	user = JSON.parse(window.localStorage.getItem('user'));
 	let routeProtected = false;
 	for(let i = 0; i < protectedRoutes.length; i++) {
 		if(window.location.href.includes(protectedRoutes[i])) {
@@ -15,23 +15,19 @@ function init() {
 	}
 	
 	// if unauthenticated user tries to access protected route, redirect to welcome page where he can login
-	if(user !== null && routeProtected) {
+	if(!user && routeProtected) {
 		window.location.replace('http://localhost:8000/index.html');
 	} else if(user && !routeProtected) { // if authenticated user tries to access login page, redirect to reservations page
 		window.location.replace('http://localhost:8000/reservations.html');
 	}
-
 }
 init();
 
 let logoutBtn = document.getElementById('logoutBtn');
 if(logoutBtn) {
 	logoutBtn.addEventListener('click', function(e) {
-		console.log('logout');
-		localStorage.removeItem('user');
+		window.localStorage.clear();
 		user = null;
-		setTimeout(function() {
-			window.location.replace('http://127.0.0.1:8000/index.html');
-		}, 100)
+		window.location.href = '/index.html';
 	});
 }
